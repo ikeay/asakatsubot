540848e13af6aaabc76babb33db33bff3de2a1fb
ar-tama <arata.makoto@gmail.com>
2015-12-23 20:34:32 +0900

    fixup! 日付割り出し

--- main.rb
+++ main.rb
@@ -1,23 +1,25 @@
+# coding: utf-8
 require 'date'
 require 'slack'
 require './key.rb'
 
+ONE_WEEK = 7
 
 Slack.configure do |config|
   config.token = TOKEN
 end
 client = Slack.realtime
 
-def date_from_wday(str)
+def date_from_wday(date_str)
     now = Date.today
     now_wday = now.wday
-    next_wday = find_wday_num(str)
+    next_wday = find_wday_num(date_str)
     if(now_wday == next_wday)
-        days_plus = 7
+        days_plus = ONE_WEEK
     elsif(now_wday < next_wday)
         days_plus = next_wday - now_wday
     elsif(now_wday > next_wday)
-        days_plus = 7 - (now_wday - next_wday)
+        days_plus = ONE_WEEK - (now_wday - next_wday)
     else
         days_plus = nil
     end
@@ -26,19 +28,19 @@ end
 
 def find_wday_num(str)
     case str
-    when "日" then
+    when '日' then
         wday_num = 0
-    when "月" then
+    when '月' then
         wday_num = 1
-    when "火" then
+    when '火' then
         wday_num = 2
-    when "水" then
+    when '水' then
         wday_num = 3
-    when "木" then
+    when '木' then
         wday_num = 4
-    when "金" then
+    when '金' then
         wday_num = 5
-    when "土" then
+    when '土' then
         wday_num = 6
     else
         wday_num = nil
@@ -69,15 +71,15 @@ end
 def push_remind_msg
     params = {
         token: TOKEN,
-        channel: "#asakatsu",
+        channel: '#asakatsu',
         as_user: true,
-        text: "明日の8:30〜朝活です。タイマーを設定しましょう！！",
+        text: '明日は朝活！8:30〜です。タイマーを設定しましょう！！',
     }
     Slack.chat_postMessage(params)
 end
 
 client.on :message do |data|
-    if bot_message?(data, "asakatsu_bot", "@U0H2JQCAU")
+    if bot_message?(data, 'asakatsu_bot', '@U0H2JQCAU')
         if wday = wday_contains?(data)
             push_reservation_msg(wday)
         end
